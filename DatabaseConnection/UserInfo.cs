730351f6781d56a640255fce621e6179a833e731
Kristopher Kram <kram.kristopher@gmail.com>
2020-11-19 11:55:09 +0100

    Userinfo

---
+++ DatabaseConnection/UserInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace DatabaseConnection
+{
+    class UserInfo
+    {
+        static void Userinfo()
+        {
+        string filePath = @"C:\Users\cream\source\repos\ViaFlix\RentalShopPoC\DatabaseConnection\UserInfo\userinfo.txt";
+
+
+
+        File.ReadAllLines(filePath);
+        
+        }
+    }
+}
