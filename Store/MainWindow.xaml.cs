db37ed91a2616fdebfad1b971fbf19de1865fc12
Alexander Fagrelius <erikalexander98@hotmail.com>
2020-12-02 15:23:10 +0100

    Bytte plats på knappar

--- Store/MainWindow.xaml.cs
+++ Store/MainWindow.xaml.cs
@@ -24,19 +24,13 @@ namespace Store
     public partial class MainWindow : Window
     {
         public MainWindow()
-        {
-          //  Movieslice();
-
+        { 
             InitializeComponent();
             Dropdown2.InitializeActionMenues(ActionPageTest);
 
             LoggedinLbl();
             HigestRated();
-            ActionList();
-            RomanceList();
-            DramaList();
-            ComedyList();
-            ScifiList();
+          
         }
 
         private void Image_MouseUp(object sender, MouseButtonEventArgs e)
@@ -58,40 +52,6 @@ namespace Store
             Namelbl.Content = $"Logged in as  \n{State.User.Username}";
         }
 
-        public void ActionList()
-        {
-            Seeding seeding = new Seeding();
-
-            
-            State.Movies = API.GetMovieSlice(0, 30);
-           
-                for (int x = 0; x < MovieGrid.ColumnDefinitions.Count; x++)
-                {
-                    int i =  MovieGrid.ColumnDefinitions.Count + x;
-                    if (i < State.Movies.Count)
-                    {
-                        var movie = State.Movies[i];
-
-                        var image = new Image() { };
-                        image.Cursor = Cursors.Hand;
-                        image.MouseUp += Image_MouseUp;
-                        image.HorizontalAlignment = HorizontalAlignment.Center;
-                        image.VerticalAlignment = VerticalAlignment.Center;
-                        image.Source = new BitmapImage(new Uri(movie.ImageURL));
-                        image.Height = 80;
-                        image.Width = 120;
-
-                        image.Margin = new Thickness(2, 2, 2, 2);
-
-                        Action.Children.Add(image);
-
-                        //Grid.SetRow(image, y);
-                        Grid.SetColumn(image, x);
-
-                    }
-                }
-           
-        }
         public void HigestRated()
         {
             State.Movies = API.GetMovieSlice(0, 30);
@@ -123,163 +83,7 @@ namespace Store
                 }
             
         }
-        public void ComedyList()
-        {
-            State.Movies = API.GetMovieSlice(0, 30);
-            
-                for (int x = 0; x < Comedy.ColumnDefinitions.Count; x++)
-                {
-                    int i = Comedy.ColumnDefinitions.Count + x;
-                    if (i < State.Movies.Count)
-                    {
-                        var movie = State.Movies[i];
-
-                        var image = new Image() { };
-                        image.Cursor = Cursors.Hand;
-                        image.MouseUp += Image_MouseUp;
-                        image.HorizontalAlignment = HorizontalAlignment.Center;
-                        image.VerticalAlignment = VerticalAlignment.Center;
-                        image.Source = new BitmapImage(new Uri(movie.ImageURL));
-                        image.Height = 80;
-                        image.Width = 120;
-
-                        image.Margin = new Thickness(2, 2, 2, 2);
-
-                        Comedy.Children.Add(image);
-
-                        
-                        Grid.SetColumn(image, x);
-
-                    }
-                }
-            
-        }
-        public void DramaList()
-        {
-            State.Movies = API.GetMovieSlice(0, 30);
-           
-                for (int x = 0; x < Drama.ColumnDefinitions.Count; x++)
-                {
-                    int i = Drama.ColumnDefinitions.Count + x;
-                    if (i < State.Movies.Count)
-                    {
-                        var movie = State.Movies[i];
-
-                        var image = new Image() { };
-                        image.Cursor = Cursors.Hand;
-                        image.MouseUp += Image_MouseUp;
-                        image.HorizontalAlignment = HorizontalAlignment.Center;
-                        image.VerticalAlignment = VerticalAlignment.Center;
-                        image.Source = new BitmapImage(new Uri(movie.ImageURL));
-                        image.Height = 80;
-                        image.Width = 120;
-
-                        image.Margin = new Thickness(2, 2, 2, 2);
-                        Drama.Children.Add(image);
-
-                        
-                        Grid.SetColumn(image, x);
-
-                    }
-                }
-            
-        }
-        public void RomanceList() 
-        {
-            State.Movies = API.GetMovieSlice(0, 30);
-           
-                for (int x = 0; x < Romance.ColumnDefinitions.Count; x++)
-                {
-                    int i = Romance.ColumnDefinitions.Count + x;
-                    if (i < State.Movies.Count)
-                    {
-                        var movie = State.Movies[i];
-
-                        var image = new Image() { };
-                        image.Cursor = Cursors.Hand;
-                        image.MouseUp += Image_MouseUp;
-                        image.HorizontalAlignment = HorizontalAlignment.Center;
-                        image.VerticalAlignment = VerticalAlignment.Center;
-                        image.Source = new BitmapImage(new Uri(movie.ImageURL));
-                        image.Height = 80;
-                        image.Width = 120;
-
-                        image.Margin = new Thickness(2, 2, 2, 2);
-
-                        Romance.Children.Add(image);
-
-                        
-                        Grid.SetColumn(image, x);
-
-                    }
-                }
-            
-        }
-        public void ScifiList() 
-        {
-            State.Movies = API.GetMovieSlice(0, 30);
-            
-                for (int x = 0; x < Sci_fi.ColumnDefinitions.Count; x++)
-                {
-                    int i =  Sci_fi.ColumnDefinitions.Count + x;
-                    if (i < State.Movies.Count)
-                    {
-                        var movie = State.Movies[i];
-
-                        var image = new Image() { };
-                        image.Cursor = Cursors.Hand;
-                        image.MouseUp += Image_MouseUp;
-                        image.HorizontalAlignment = HorizontalAlignment.Center;
-                        image.VerticalAlignment = VerticalAlignment.Center;
-                        image.Source = new BitmapImage(new Uri(movie.ImageURL));
-                        image.Height = 80;
-                        image.Width = 120;
-
-                        image.Margin = new Thickness(2, 2, 2, 2);
-
-                        Sci_fi.Children.Add(image);
-
-                        
-                        Grid.SetColumn(image, x);
-
-                    }
-                }
-            
-        }
-     /*   public void Movieslice()
-        {
-            using (var ctx = new Context())
-            {
-                
-                ctx.RemoveRange(ctx.Movies);
-                ctx.RemoveRange(ctx.Sales);
-
-
-                var movies = new List<Movie>();
-                var lines = File.ReadAllLines(@"..\..\..\..\DatabaseConnection\SeedData\MovieGenre.csv");
-                var genre_lines = File.ReadAllLines(@"..\..\..\..\DatabaseConnection\SeedData\MovieGenre.csv");
-                for (int i = 1; i < 200; i++)
-                {
-                    // imdbId,Imdb Link,Title,IMDB Score,Genre,Poster
-                    var cells = lines[i].Split(',');
-                    var url = cells[5].Trim('"');
-
-                    // Hämta film genres
-                    string[] genre_cells = genre_lines[i].Split(',');
-                    string genre = genre_cells[4];
-
-                    // Hoppa över alla icke-fungerande url:er
-                    try { var test = new Uri(url); }
-                    catch (Exception e) { continue; }
-
-                    movies.Add(new Movie { Title = cells[2], ImageURL = url });
-                }
-                ctx.AddRange(movies);
-
-                ctx.SaveChanges();
-            }
-        }
-     */
+       
   
         private void HomeButton_Click(object sender, RoutedEventArgs e)
         {
